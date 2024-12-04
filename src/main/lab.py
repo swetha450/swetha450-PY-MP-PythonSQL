7fea27bff90940a138d5bc780dd306650c03e67c
swetha450 <swetha70751@gmail.com>
2024-12-04 22:47:15 +0530

    done

--- src/main/lab.py
+++ src/main/lab.py
@@ -5,35 +5,43 @@ as well as creating a table, inserting data, and selecting that data.
 import sqlite3
 
 
-conn = sqlite3.connect('dogs.db')
+conn = sqlite3.connect(":memory:")
 cursor = conn.cursor()
 
 
 # Create a dogs table with autoincrementing ID
 def create_dogs_table():
-
-    cursor.execute('''
-    create table dogs(id integer primary key autoincrement,
-    name text not null,
-    breed text not null,
-    age integer not null);
-    ''')
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS dogs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            breed TEXT NOT NULL,
+            age INTEGER NOT NULL
+        )
+    """)
     conn.commit()
 
-    
-    
-    """TODO"""
+   
 
 
 # TODO: Complete insert_dog() by inserting a new dog (provided in the parameters) into the "dogs" table.
 def insert_dog(name, breed, age):
-    cursor.execute(''' insert into dogs(name, breed, age) values(?,?,?);''',(name, breed, age))
+    cursor.execute("""
+        INSERT INTO dogs (name, breed, age)
+        VALUES (?, ?, ?)
+    """, (name, breed, age))
     conn.commit()
 
+    
+    
 
 
 # TODO: Complete select_all_dogs() by selecting all rows from the "dogs" table *and returning them*.
 def select_all_dogs():
-    cursor.execute('select * from dogs;')
+    cursor.execute("""
+        SELECT * FROM dogs
+    """)
     rows = cursor.fetchall()
+
+    # return the rows
     return rows
